c88cb972cd6a4bc0a086c1695338649db8e49ef5
Chorizo <mateoledru05@gmail.com>
2024-02-07 15:27:49 +0100

    Push

---
+++ PFA-Projet Racine/Assets/Script/HumanList.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanList : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject panel_human;
+    [SerializeField]
+    private GameObject Button_hide;
+    [SerializeField]
+    private GameObject Button_show;
+
+    public void Human_Liste()
+    {
+        panel_human.SetActive(true);
+        Button_hide.SetActive(true);
+        Button_show.SetActive(false);
+    }
+
+    public void Hide_Human_List()
+    {
+        panel_human.SetActive(false);
+        Button_hide.SetActive(false);
+        Button_show.SetActive(true);
+
+    }
+}
