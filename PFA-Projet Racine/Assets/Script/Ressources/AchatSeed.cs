5d7864b99e191765eae8e0f6fdc929e822b12c9b
Chorizo <mateoledru05@gmail.com>
2024-05-14 16:21:16 +0200

    ue-(ui(djsryyq<zus-rir-èlosisxis
    jte

--- PFA-Projet Racine/Assets/Script/Ressources/AchatSeed.cs
+++ PFA-Projet Racine/Assets/Script/Ressources/AchatSeed.cs
@@ -1,16 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class AchatSeed : MonoBehaviour
 {
     public RessourceManager RessourceManager;
+    public TextMeshProUGUI textWood;
+    public TextMeshProUGUI textWood2;
+    public TextMeshProUGUI textStone;
+    public TextMeshProUGUI textO;
+    public TextMeshProUGUI textO2;
     private int index = 1;
 
+    public void Start()
+    {
+        string wood = "20";
+        string water = "20";
+        textWood.text = wood.ToString();
+        textO.text = water.ToString();
+    }
     public void UpgradeRoot()
     {
         if (index == 1)
         {
+            string wood = "35";
+            string stone = "15";
+            string water = "50";
+            textWood2.text = wood.ToString();
+            textStone.text = stone.ToString();
+            textO2.text = water.ToString();
             RessourceManager.EditWoodAmount(-20);
             RessourceManager.EditWaterAmount(-30);
             index = 2;
