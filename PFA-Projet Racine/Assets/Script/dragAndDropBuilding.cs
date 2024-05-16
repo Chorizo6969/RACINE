321a7b3f06c79b72f3dc0f3a13e4dbecd69317dd
Chorizo <mateoledru05@gmail.com>
2024-05-16 10:26:22 +0200

    DFHSTRJTR

--- PFA-Projet Racine/Assets/Script/dragAndDropBuilding.cs
+++ PFA-Projet Racine/Assets/Script/dragAndDropBuilding.cs
@@ -50,6 +50,7 @@ public class dragAndDropBuilding : MonoBehaviour
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hitInfo;
             Physics.Raycast(ray, out hitInfo);
+            Debug.Log(hitInfo.collider.gameObject.name);
             if (BOUGE != null)
             {
                 if (hitInfo.collider != null)
