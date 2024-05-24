e771dffa99dc640d0266f64ebb034fa49efd9b10
Chorizo <mateoledru05@gmail.com>
2024-05-24 14:02:56 +0200

    Fin de Title screen

--- PFA-Projet Racine/Assets/Play.cs
+++ PFA-Projet Racine/Assets/Play.cs
@@ -1,12 +1,14 @@
 using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine.UI;
 
 public class Play : MonoBehaviour
 {
     [SerializeField]
-    private Animator _animator;
+    private Animation _anim;
+
+    [SerializeField]
+    private Animation _anim2;
 
     [SerializeField]
     private Button _button1;
@@ -20,14 +22,13 @@ public class Play : MonoBehaviour
     [SerializeField] 
     private Button _button4;
 
-    [SerializeField]
-    private GameObject _panel;
+    private int _id = 0;
 
 
     public void StartAnim()
     {
-        _animator.SetBool("IsBack", false);
-        _animator.SetBool("IsPlay", true);
+        _id = 0;
+        _anim.Play("Slide");
         _button1.interactable = false;
         _button2.interactable = false;
         _button3.interactable = false;
@@ -35,17 +36,33 @@ public class Play : MonoBehaviour
         StartCoroutine(Delay());
     }
 
-    public void panel2()
+    public void Transition()
     {
-        _animator.SetBool("IsBack", true);
-        _animator.SetBool("IsPlay", false);
+
     }
+
+    public void Back()
+    {
+        _id = 1;
+        _anim2.Play("ReverseSlide2");
+        StartCoroutine(Delay());
+    }
+
     IEnumerator Delay()
     {
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(0.7f);
         _button1.interactable = true;
         _button2.interactable = true;
         _button3.interactable = true;
         _button4.interactable = true;
+        if (_id == 0)
+        {
+            _anim2.gameObject.SetActive(true);
+            _anim2.Play("Slide2");
+        }
+        else
+        {
+            _anim.Play("ReverseSlide");
+        }
     }
 }
