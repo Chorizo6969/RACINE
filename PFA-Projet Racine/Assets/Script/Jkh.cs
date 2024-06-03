5690534cf350b273254a8f4b445247d7ceb342a4
Lucas Pécout <lucas.pecout@e-artsup.net>
2024-06-03 17:43:26 +0200

    Tornado ADV

--- PFA-Projet Racine/Assets/Script/Jkh.cs
+++ PFA-Projet Racine/Assets/Script/Jkh.cs
@@ -4,8 +4,22 @@ using UnityEngine;
 
 public class Jkh : MonoBehaviour
 {
-    public bool booliste;
-    public bool BOOLISTE;
+    public bool OriginalBooliste;
+
+    public bool boolisteBuildingPanel;
+    public bool BOOLISTEBUILDINGPANEL;
+
+    public bool boolisteOptionPanel;
+    public bool BOOLISTEOPTIONPANEL;
+
+    public bool boolisteExpedBoisPanel;
+    public bool BOOLISTEEXPEDBOISPANEL;
+
+    public bool boolisteExpedEauPanel;
+    public bool BOOLISTEEXPEDEAUPANEL;
+
+    public bool boolisteExpedPierrePanel;
+    public bool BOOLISTEEXPEDPIERREPANEL;
 
     public static Jkh Instance;
 
@@ -13,18 +27,99 @@ public class Jkh : MonoBehaviour
     {
         Instance = this;
     }
-    public void OnClick()
+    public void OnClickBuildingPanel()
     {
-        booliste = true;
-        if (booliste && !BOOLISTE)
+        boolisteBuildingPanel = true;
+        if (boolisteBuildingPanel && !BOOLISTEBUILDINGPANEL)
         {
-            booliste = false;
-            BOOLISTE = true;
+            boolisteBuildingPanel = false;
+            BOOLISTEBUILDINGPANEL = true;
         }
-        else if (booliste && BOOLISTE)
+        else if (boolisteBuildingPanel && BOOLISTEBUILDINGPANEL)
         {
-            booliste = false;
-            BOOLISTE = false;
+            boolisteBuildingPanel = false;
+            BOOLISTEBUILDINGPANEL = false;
         }
+    }
+
+    public void OnClickOptionPanel()
+    {
+        boolisteOptionPanel = true;
+        if (boolisteOptionPanel && !BOOLISTEOPTIONPANEL)
+        {
+            boolisteOptionPanel = false;
+            BOOLISTEOPTIONPANEL = true;
+        }
+        else if (boolisteOptionPanel && BOOLISTEOPTIONPANEL)
+        {
+            boolisteOptionPanel = false;
+            BOOLISTEOPTIONPANEL = false;
+        }
+    }
+
+    public void OnClickExpedBoisPanel()
+    {
+        boolisteExpedBoisPanel = true;
+        if (boolisteExpedBoisPanel && !BOOLISTEEXPEDBOISPANEL)
+        {
+            boolisteExpedBoisPanel = false;
+            BOOLISTEEXPEDBOISPANEL = true;
+        }
+        else if (boolisteExpedBoisPanel && BOOLISTEEXPEDBOISPANEL)
+        {
+            boolisteExpedBoisPanel = false;
+            BOOLISTEEXPEDBOISPANEL = false;
+        }
+    }
+
+    public void OnClickExpedEauPanel()
+    {
+        boolisteExpedEauPanel = true;
+        if (boolisteExpedEauPanel && !BOOLISTEEXPEDEAUPANEL)
+        {
+            boolisteExpedEauPanel = false;
+            BOOLISTEEXPEDEAUPANEL = true;
+        }
+        else if (boolisteExpedEauPanel && BOOLISTEEXPEDEAUPANEL)
+        {
+            boolisteExpedEauPanel = false;
+            BOOLISTEEXPEDEAUPANEL = false;
+        }
+    }
+
+    public void OnClickExpedPierrePanel()
+    {
+        boolisteExpedPierrePanel = true;
+        if (boolisteExpedPierrePanel && !BOOLISTEEXPEDEAUPANEL)
+        {
+            boolisteExpedPierrePanel = false;
+            BOOLISTEEXPEDEAUPANEL = true;
+        }
+        else if (boolisteExpedPierrePanel && BOOLISTEEXPEDEAUPANEL)
+        {
+            boolisteExpedPierrePanel = false;
+            BOOLISTEEXPEDEAUPANEL = false;
+        }
+    }
+
+    /*public void OnClickExpedPierrePanel()
+    {
+        boolisteExpedPierrePanel = true;
+        if (boolisteExpedPierrePanel && !BOOLISTEEXPEDEAUPANEL)
+        {
+            boolisteExpedPierrePanel = false;
+            BOOLISTEEXPEDEAUPANEL = true;
+        }
+        else if (boolisteExpedPierrePanel && BOOLISTEEXPEDEAUPANEL)
+        {
+            boolisteExpedPierrePanel = false;
+            BOOLISTEEXPEDEAUPANEL = false;
+        }
+    }*/
+
+    public bool CheckAllBool()
+    {
+        OriginalBooliste = boolisteBuildingPanel && boolisteOptionPanel && boolisteExpedBoisPanel && boolisteExpedEauPanel && boolisteExpedPierrePanel;
+        return OriginalBooliste;
     }
 }
