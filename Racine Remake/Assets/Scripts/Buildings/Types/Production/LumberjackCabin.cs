f8b55d620e89a1d3aab192a266e5d4d506fca7af
JElouann <eljouenne@gmail.com>
2025-09-04 03:05:58 +0200

    tree radial detection

--- Racine Remake/Assets/Scripts/Buildings/Types/Production/LumberjackCabin.cs
+++ Racine Remake/Assets/Scripts/Buildings/Types/Production/LumberjackCabin.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class LumberjackCabin : WorkBase
@@ -6,25 +7,29 @@ public class LumberjackCabin : WorkBase
     [SerializeField] private float _treeDetectionRadius;
     [SerializeField] private LayerMask _cellLayer;
     private Collider[] _results;
+    [SerializeField] private List<ProtoTree> _trees = new();
 
     private void Start()
     {
-        //StartCoroutine(CheckTreeCoroutine());
+        StartCoroutine(CheckTreeCoroutine());
     }
 
     private IEnumerator CheckTreeCoroutine()
     {
+        _results = new Collider[500];
         while (true)
         {
             int hitCount = Physics.OverlapSphereNonAlloc(transform.position, _treeDetectionRadius, _results, _cellLayer);
             for (int i = 0; i < hitCount; i++)
             {
-                if (_results[i].TryGetComponent(out Cell cellComponent))
-                {
-                    print(cellComponent.transform.position);
-                }
+                if (!_results[i].TryGetComponent(out Cell cell)) continue;
+                if (!cell.Building) continue;
+                if (!cell.Building.TryGetComponent(out ProtoTree tree)) continue;
+                if (_trees.Contains(tree)) continue;
+               
+                _trees.Add(tree);
             }
-            yield return new WaitForSeconds(0.5f);
+            yield return new WaitForSeconds(1f);
         }
     }
 
@@ -32,5 +37,12 @@ public class LumberjackCabin : WorkBase
     {
         Gizmos.color = Color.cyan;
         Gizmos.DrawWireSphere(this.transform.position, _treeDetectionRadius);
+        Gizmos.color = Color.magenta;
+        foreach (var tree in _trees)
+        {
+            if (!tree) return;
+            Ray ray = new Ray(tree.transform.position, transform.up * 6);
+            Gizmos.DrawRay(ray);
+        }
     }
 }
